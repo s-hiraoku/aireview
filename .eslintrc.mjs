410d32c3d58959fa8652b59130f96e8372dd4b31
hiraoku.shinichi <s.hiraoku@gmail.com>
2023-09-09 13:39:50 +0900

    Rename .eslintrc.cjs to .eslintrc.mjs and update the file references in eslint configuration

--- .eslintrc.mjs
+++ .eslintrc.mjs
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   overrides: [
@@ -9,7 +10,7 @@ module.exports = {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: [".eslintrc.{js,mjs}"],
       parserOptions: {
         sourceType: "script",
       },
