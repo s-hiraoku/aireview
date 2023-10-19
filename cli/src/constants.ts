0ac590018bdffeacae82bff6a5396983966eba24
hiraoku.shinichi <s.hiraoku@gmail.com>
2023-10-20 07:19:07 +0900

    add .env setup features

--- cli/src/constants.ts
+++ cli/src/constants.ts
@@ -2,3 +2,4 @@ export const DIFF_EXTENSION = '.diff';
 export const ZIP_EXTENSION = '.zip';
 export const DIFF_FILES_DIR = 'git-diff-files';
 export const ZIP_FILE_NAME = 'diff.zip';
+export const ENV_FILE_NAME = '.env';
